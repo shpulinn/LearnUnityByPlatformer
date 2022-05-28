12f13971f2adba36e18841bec61e22f91420b8a6
Nikita <shpulinn@mail.ru>
2022-05-28 14:15:57 +0200

    Update PlayerHealth.cs

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,30 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-    [SerializeField] private float health = 100.0f;
+    [SerializeField] private float totalHealth = 100.0f;
+    [SerializeField] private Slider sliderHealth;
     private Animator _animator;
 
     private int _takeDamageAnimTrigger;
+    private float _health;
 
     private void Awake()
     {
         _animator = GetComponentInChildren<Animator>();
 
         _takeDamageAnimTrigger = Animator.StringToHash("TakeDamagePlayer");
+
+        _health = totalHealth;
+        UpdateHealth();
     }
 
     public void ReduceHealth(float damage)
     {
-        health -= damage;
+        _health -= damage;
         _animator.SetTrigger(_takeDamageAnimTrigger);
-        if (health <= 0.0f) Die();
+
+        UpdateHealth();
+        
+        if (_health <= 0.0f) Die();
+        
     }
 
     private void Die()
     {
         this.gameObject.SetActive(false);
+    }
+
+    private void UpdateHealth()
+    {
+        sliderHealth.value = _health / totalHealth;
     }
 }
