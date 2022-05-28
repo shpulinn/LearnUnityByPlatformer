f0d9bd748190d48ad5e37a1bcea965bc4ff4d241
Nikita <shpulinn@mail.ru>
2022-05-28 14:15:50 +0200

    Update EnemyHealth.cs

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -2,30 +2,45 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour
 {
-    [SerializeField] private float health = 100.0f;
+    [SerializeField] private float totalHealth = 100.0f;
+    [SerializeField] private Slider healthSlider;
     private Animator _animator;
 
+    private float _health;
+    
     private int _takeDamageAnimTrigger;
 
     private void Awake()
     {
-        _animator = GetComponent<Animator>();
+        _animator = GetComponentInChildren<Animator>();
 
         _takeDamageAnimTrigger = Animator.StringToHash("TakeDamage");
+
+        _health = totalHealth;
+        UpdateHealth();
     }
 
     public void ReduceHealth(float damage)
     {
-        health -= damage;
+        _health -= damage;
         _animator.SetTrigger(_takeDamageAnimTrigger);
-        if (health <= 0.0f) Die();
+
+        UpdateHealth();
+        
+        if (_health <= 0.0f) Die();
     }
 
     private void Die()
     {
         this.gameObject.SetActive(false);
+    }
+
+    private void UpdateHealth()
+    {
+        healthSlider.value = _health / totalHealth;
     }
 }
